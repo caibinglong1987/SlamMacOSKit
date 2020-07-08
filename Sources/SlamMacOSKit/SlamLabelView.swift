d0d441232c665eac515b2c62c266483572162b7c
Steve Sheets <magesteve@mac.com>
2020-07-08 19:00:06 -0400

    Label

---
+++ Sources/SlamMacOSKit/SlamLabelView.swift
@@ -0,0 +1,78 @@
+//
+//  SlamLabelView.swift
+//  SlamCocoa
+//
+//  Created by Sheets, Steve on 5/27/17.
+//  Copyright © 2017 Sheets, Steve. All rights reserved.
+//
+
+import Cocoa
+import SlamKit
+
+// MARK: - Class
+
+/// Slam Based Label style Text Field
+public class SlamLabelView: NSTextField, SlamReferable, SlamVisibleable, SlamEnableable, SlamLabelable {
+    
+    // MARK: - SlamReferable requirements
+    
+    @IBInspectable public var slamReferral: String = ""
+
+    @IBInspectable public var slamWord: String = ""
+
+    @IBInspectable public var slamTag: Int = 0
+
+    // MARK: - SlamVisibleable Requirements
+    
+    public var slamVisibleState: Bool {
+        get {
+            !isHidden
+        }
+        set(value) {
+            if isHidden != !value {
+                isHidden = !value
+            }
+        }
+    }
+    
+    public var slamVisibleDataSource: SlamKit.ActionBoolClosure?
+
+    public func slamUpdateUI() {
+        var ui = self
+        
+        ui.slamUpdateVisible()
+        ui.slamUpdateEnable()
+        ui.slamUpdateLabel()
+    }
+
+    // MARK: - SlamEnableable Requirements
+    
+    public var slamEnabledState: Bool {
+        get {
+            isEnabled
+        }
+        set(value) {
+            isEnabled = value
+        }
+    }
+
+    public var slamEnableDataSource: SlamKit.ActionBoolClosure?
+
+    // MARK: - SlamLabelable Requirements
+    
+    public var slamLabeledState: String {
+        get {
+            stringValue
+        }
+        set(value) {
+            if value != stringValue {
+                stringValue = value
+            }
+        }
+    }
+
+    public var slamLabelDataSource: SlamKit.ActionStringClosure?
+
+}
+
+
--- Sources/SlamMacOSKit/SlamMacOSKit.swift
+++ Sources/SlamMacOSKit/SlamMacOSKit.swift
@@ -1,3 +1,11 @@
+//
+//  SlamMacOSKit.h
+//  SlamMacOSKit
+//
+//  Created by Steve Sheets on 7/8/20.
+//  Copyright © 2020 Steve Sheets. All rights reserved.
+//
+
 // MARK: Structure
 
 /// Abstract Structrure
