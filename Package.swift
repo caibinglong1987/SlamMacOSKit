bea9d818dbab12e9cac2ba87f1c73e8ca8570684
Steve Sheets <magesteve@mac.com>
2020-07-12 21:11:06 -0400

    update Slamkit

--- Package.swift
+++ Package.swift
@@ -9,18 +9,14 @@ let package = Package(
         .macOS(.v10_13)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SlamMacOSKit",
             targets: ["SlamMacOSKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/magesteve/SlamKit.git", from: "0.0.1"),
+        .package(url: "https://github.com/magesteve/SlamKit.git", from: "0.0.4"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SlamMacOSKit",
             dependencies: ["SlamKit"]),
